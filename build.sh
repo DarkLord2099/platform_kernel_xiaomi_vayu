347152e5ecea92e651f867570383bed50a35f332
DarkLord2099 <149946506+DarkLord2099@users.noreply.github.com>
2024-04-29 00:39:56 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,12 +8,12 @@ objdir="${kernel_dir}/out"
 anykernel=$HOME/anykernel
 builddir="${kernel_dir}/build"
 ZIMAGE=$kernel_dir/out/arch/arm64/boot/Image
-kernel_name="SkylineKernel_vayu_"
+kernel_name="SpartaKernel_vayu_"
 zip_name="$kernel_name$(date +"%Y%m%d").zip"
 CLANG_DIR=$HOME/tc/clang-r498229b
 export CONFIG_FILE="vayu_user_defconfig"
 export ARCH="arm64"
-export KBUILD_BUILD_HOST=gxc2356
+export KBUILD_BUILD_HOST=yfmarco
 export KBUILD_BUILD_USER=home
 
 export PATH="$CLANG_DIR/bin:$PATH"
